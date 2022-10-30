bac00cee048879719671153b0f23ce7b747c2fc2
Dylan Moore <alloydylan@icloud.com>
2022-10-29 20:10:02 -0400

    dist test

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='autodd',
-    version='1.0.1',
+    version='1.0.0',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     install_requires=[
